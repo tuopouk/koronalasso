e54e0e443e8eb37365cefc986f48cbbe9244fdd9
Poukkula Tuomas <tuomas.poukkula@turku.fi>
2020-05-10 19:37:05 +0300

    'parempiKieli'

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ def serve_layout():
                     html.Label(['Lassoregression dokumentaatio: ', html.A('Scikit-Learn', href='https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Lasso.html#')]),
                     html.Label(['Regressiometriikoiden dokumentaatio: ', html.A('Scikit-Learn', href='https://scikit-learn.org/stable/modules/model_evaluation.html#regression-metrics')]),
                     html.Label(['Katso toteutuneet koronatiedot ', html.A('täältä.', href='http://bit.ly/turkukorona'),
-                    html.Label(['by Tuomas Poukkula ', html.A('Twitter', href='https://twitter.com/TuomasPoukkula')]),
+                    html.Label(['Tehnyt Tuomas Poukkula ', html.A('Seuraa Twitterissä.', href='https://twitter.com/TuomasPoukkula')]),
                     ])
         
         
@@ -315,7 +315,7 @@ def ennusta(shp,days):
                                                                            name='Ennuste')
                                                                ],
                                                           layout=go.Layout(title = dict(
-                                                          text = 'Koronatartuntojen seuraavan '+str(days)+' päivän kumulatiivinen ennuste alueelle: '+shp,
+                                                          text = 'Koronavirustartuntojen seuraavan '+str(days)+' päivän kumulatiivinen ennuste alueelle: '+shp,
                                                                                    y = 0.9,
                                                                                    x = 0.5,
                                                                                    xanchor = 'center',
@@ -343,7 +343,7 @@ def ennusta(shp,days):
                                                                        textposition='outside'
                                                                        )],
                                                            layout = go.Layout(title = dict(
-                                                                    text = 'Koronatartuntojen seuraavan '+str(days)+' päivän päivittäiset ennusteet alueelle: '+shp,
+                                                                    text = 'Koronavirustartuntojen seuraavan '+str(days)+' päivän päivittäiset ennusteet alueelle: '+shp,
                                                                                    y = 0.9,
                                                                                    x = 0.5,
                                                                                    xanchor = 'center',
